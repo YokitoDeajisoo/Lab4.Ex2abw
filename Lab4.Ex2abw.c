786eeebb56c7f6613293d5c4aba900e24375bde5
Honcharuk Oleksii <oleksiugoncaruk@gmail.com>
2025-06-04 12:40:11 +0300

    Update Lab4.Ex2abw.c

--- Lab4.Ex2abw.c
+++ Lab4.Ex2abw.c
@@ -1,37 +1,14 @@
-﻿#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <stdlib.h>
-#include <clocale>
-
-void for_cycle() {
-    int sum = 0;
-    for (int i = 2; i <= 20; i += 2) {
-        sum += i * i;
-    }
-    printf("Сума квадратiв парних чисел [1;20] (for): %d\n", sum);
-}
-
-void while_cycle() {
-    int i = 2, sum = 0;
-    while (i <= 20) {
-        sum += i * i;
-        i += 2;
-    }
-    printf("Сума квадратiв парних чисел [1;20] (while): %d\n", sum);
-}
-
-void do_while_cycle() {
-    int i = 2, sum = 0;
-    do {
-        sum += i * i;
-        i += 2;
-    } while (i <= 20);
-    printf("Сума квадратiв парних чисел [1;20] (do-while): %d\n", sum);
-}
+#include <stdlib.h> 
+#include <clocale>  
 
 int main() {
     setlocale(LC_ALL, "Ukrainian");
     int choice;
+    int result;
+    int i, sum;
+
     do {
         printf("\n=== Меню ===\n");
         printf("1. Виконати з циклом for\n");
@@ -43,17 +20,37 @@ int main() {
 
         switch (choice) {
         case 1:
-            for_cycle();
+            sum = 0;
+            for (i = 2; i <= 20; i += 2) {
+                sum += i * i;
+            }
+            printf("Сума квадратiв парних чисел [1;20] (for): %d\n", sum);
             break;
+
         case 2:
-            while_cycle();
+            sum = 0;
+            i = 2;
+            while (i <= 20) {
+                sum += i * i;
+                i += 2;
+            }
+            printf("Сума квадратiв парних чисел [1;20] (while): %d\n", sum);
             break;
+
         case 3:
-            do_while_cycle();
+            sum = 0;
+            i = 2;
+            do {
+                sum += i * i;
+                i += 2;
+            } while (i <= 20);
+            printf("Сума квадратiв парних чисел [1;20] (do-while): %d\n", sum);
             break;
+
         case 0:
             printf("Завершення програми.\n");
             break;
+
         default:
             printf("Невірний вибір. Спробуйте ще раз.\n");
         }
